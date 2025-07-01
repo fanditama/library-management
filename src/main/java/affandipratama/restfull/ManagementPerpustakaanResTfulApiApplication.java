ef1c7bb6cfa93b9067169d023ff8149e67e9009a
Affandi Pratama <afandip911@gmail.com>
2025-07-01 14:38:17 +0700

    first commit

---
+++ src/main/java/affandipratama/restfull/ManagementPerpustakaanResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package affandipratama.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementPerpustakaanResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementPerpustakaanResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/affandipratama/restfull/ManagementPerpustakaanResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package affandipratama.restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementPerpustakaanResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
